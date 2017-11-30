e8e52c7a7318774fe6d94a4afd2a0bd7192ad278
Richard Hibbitts <richard.hibbitts@ingramcontent.com>
2017-11-30 16:24:44 -0500

    Section 9 More dynamic components

--- section_9/src/components/New.vue
+++ section_9/src/components/New.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
         <h3>New Quote</h3>
+        <button @click="counter++">Increase</button>
+        <p>{{ counter }}</p>
     </div>
 </template>
 
 <script>
 export default {
-  
+    data: function() {
+        return {
+            counter: 0
+        };
+    }
 }
 </script>
 
