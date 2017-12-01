4cd8cc8aa90f0d94c70f2c2eca20587209b3e90f
Richard Hibbitts <richard.hibbitts@ingramcontent.com>
2017-12-01 17:09:21 -0500

    Section 10: Finished Quotes App

--- section_10/src/App.vue
+++ section_10/src/App.vue
@@ -1,10 +1,21 @@
 <template>
     <div class="container">
-    
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a box to delete it.</div>
+            </div>
+        </div> 
     </div>
 </template>
 
 <script>
+    import QuoteGrid from "./components/QuoteGrid.vue";
+    import NewQuote from "./components/NewQuote.vue";
+    import Header from "./components/Header.vue";
+    
     export default {
         data: function() {
             return {
@@ -13,7 +24,23 @@
                 ],
                 maxQuotes: 10
             };
-        }        
+        },
+        components:{
+            quoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        },
+        methods: {
+            newQuote(quote){
+                if (this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete quotes first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index,1);
+            }
+        }      
     }
 </script>
 
